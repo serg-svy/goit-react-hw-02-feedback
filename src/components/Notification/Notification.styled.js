df9235db8b171ee5bcd739728d3d7cdd27e98a98
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-11-01 23:09:57 +0200

    push

--- src/components/Notification/Notification.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wraper = styled.div`
-margin: 20px 0 0 20px;
-font-family: -apple - system, system - ui, "Segoe UI", Helvetica, Arial, sans - serif, "Apple Color Emoji", "Segoe UI Emoji";
-font-size: 16px;
-font-weight: 500;
-line-height: 22px;
-`
