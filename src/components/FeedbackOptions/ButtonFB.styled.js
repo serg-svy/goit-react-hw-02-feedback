a437dbb6e3edea89324aec2d5a1bb265d292a6c2
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-11-02 08:23:17 +0200

    push

--- src/components/FeedbackOptions/ButtonFB.styled.js
+++ src/components/FeedbackOptions/ButtonFB.styled.js
@@ -15,7 +15,7 @@ export const Button = styled.button`
   height: 80px;
   border-radius: 30%;
   border: 5px solid white;
-  background-color: #444443;
+  background-color: #444444;
   color: white;
   font-size: 16px;
   font-weight: 700;
